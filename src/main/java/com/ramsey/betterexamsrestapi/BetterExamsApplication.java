8d6ade437bdcc026e1fc7833e73cd53f527b17e6
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2021-09-27 14:05:55 +0200

    Adding dependencies the pom file. Adding database properties to the application properties file. Adding mail jet keys to the email properties file.

---
+++ src/main/java/com/ramsey/betterexamsrestapi/BetterExamsApplication.java
@@ -0,0 +1,15 @@
+package com.ramsey.betterexamsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterExamsApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(BetterExamsApplication.class, args);
+		
+	}
+	
+}
